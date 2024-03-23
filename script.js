483b524fd389587b6be0107306f6634e5b178c85
eldin001 <eldin.balje@icloud.com>
2024-03-23 13:20:55 +0100

    added array of object

--- script.js
+++ script.js
@@ -25,6 +25,59 @@ const listaOggettiInvitati = invitati.map((guestName, index) => {
 
 console.log(listaOggettiInvitati);
 
+/* Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+ES (Marco della Rovere => MARCO DELLA ROVERE);
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+Buon pomeriggio e buon lavoro !!! :faccia_nerd:
+Questo è l'elenco degli studenti:
+Id  Name                Grades
+213 Marco della Rovere      78
+110 Paola Cortellessa       96
+250 Andrea Mantegna         48
+145 Gaia Borromini          74
+196 Luigi Grimaldello       68
+102 Piero della Francesca   50
+120 Francesca da Polenta    84 */
+
+const studenti = [
+    { 
+        id: 213, 
+        name: 'Marco della Rovere',
+        grades: 78 
+    },
+    { 
+        id: 110, 
+        name: 'Paola Cortellessa',
+         grades: 96 
+    },
+    { 
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    { 
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    { 
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68 
+    },
+    { 
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50
+    },
+    { 
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    }
+];
 
 
 
