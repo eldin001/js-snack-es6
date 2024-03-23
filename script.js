a93156d33291e29b8b7544c9a892e7040c6061f9
eldin001 <eldin.balje@icloud.com>
2024-03-23 13:14:02 +0100

    list invited

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
+[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+Ma la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto javascript che ha come attributi:
+nome del tavolo (tableName),
+nome dell'ospite (guestName),
+posto occupato (place),
+Generiamo e stampiamo in console la lista per i segnaposto. */
+
+const invitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+
+let tableName = 'Tavolo Vip';
+
+newObjectFunction = (tableName, guestName, place) => {
+    return {
+        nomeDelTavolo : tableName,
+        nomeInvitato : guestName,
+        posizione : place
+    };
+};
+
+const listaOggettiInvitati = invitati.map((guestName, index) => {
+    return newObjectFunction(tableName, guestName, index + 1);
+});
+
+console.log(listaOggettiInvitati);
+
+
+
+
